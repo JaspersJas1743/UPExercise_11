d5f093cb98f31aa09235c1e15c1755a3efc803f4
JaspersJas1743 <lesha.smrinov2019@mail.ru>
2022-12-06 18:32:19 +0300

    [+] update for work with file

--- Задание №11/Assets/MyWindows/OpenFileWindow.xaml.cs
+++ Задание №11/Assets/MyWindows/OpenFileWindow.xaml.cs
@@ -1,35 +1,24 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Input;
+using Задание__11.Assets.Utility;
 
 namespace Задание__11.Assets.MyWindows
 {
-	public partial class OpenFileWindow : Window
+    public partial class OpenFileWindow : Window
 	{
-		private const string _defaultFileName = "Безымянный.txt";
-
 		public OpenFileWindow()
 			=> InitializeComponent();
 
-		public string EnteredEncodingStringFormat { get; private set; } = String.Empty;
-		public Encoding EnteredEncoding { get; private set; } = Encoding.Default;
-		public string EnteredFileName { get; private set; } = String.Empty;
-		public string EnteredFileFullName { get; private set; } = String.Empty;
-
-		private void SetEncoding(Encoding encoding, MenuItem menuItem)
+		private void DragMove(object sender, MouseButtonEventArgs e)
 		{
-			EnteredEncoding = encoding;
-			MainHeader.Header = menuItem.Header;
-			EnteredEncodingStringFormat = menuItem.Header.ToString();
+			if(Mouse.LeftButton == MouseButtonState.Pressed)
+				this.DragMove();
 		}
 
-		private void Windows1251Click(object sender, RoutedEventArgs e)
-			=> SetEncoding(encoding: Encoding.GetEncoding("windows-1251"), menuItem: (MenuItem)sender);
-
-		private void UnicodeClick(object sender, RoutedEventArgs e)
-			=> SetEncoding(encoding: Encoding.Unicode, menuItem: (MenuItem)sender);
+		private void DeactivateClick(object sender, RoutedEventArgs e)
+			=> this.WindowState = WindowState.Minimized;
 
 		private void ClickForSelectFile(object sender, RoutedEventArgs e)
 		{
@@ -37,19 +26,23 @@ namespace Задание__11.Assets.MyWindows
 			{
 				FileName = String.Empty,
 				DefaultExt = ".rtf",
-				Filter = "Текстовый файл|*.txt|Файлы RTF|*.rtf",
-				Title = "Открыть файл для редактирования",
+				Filter = "Текстовый файл|*.txt|Файл RTF|*.rtf|Бинарный файл|*.bin",
+				Title = "Открыть файл",
 				ValidateNames = true
 			};
-			if(openFile.ShowDialog() ?? false)
+			if(!openFile.ShowDialog() ?? false)
 			{
-				EnteredFileName = FileNameBox.Text = openFile.SafeFileName;
-				EnteredFileFullName = openFile.FileName;
-			} else
-				EnteredFileFullName = EnteredFileName = FileNameBox.Text = _defaultFileName;
+				MyMessageWindow.Show(text: "Выберите файл!", pathToImage: @"\Assets\Images\error.png");
+				return;
+			}
+			PickedFile.FileName = FileNameBox.Text = openFile.SafeFileName;
+			PickedFile.FileFullName = openFile.FileName;
+			if(PickedFile.FileName.EndsWith(value: ".txt"))
+				UICreator.CreateSelectionEncoding(grid: MainGrid, button: SelectedButton, window: this);
 		}
 
 		private void SelectedClick(object sender, RoutedEventArgs e)
-			=> DialogResult = !String.IsNullOrEmpty(EnteredFileName) && Encoding.Default != EnteredEncoding;
+			=> DialogResult = !String.IsNullOrWhiteSpace(value: PickedFile.FileName);
+
 	}
 }
---
+++ Задание №11/Assets/Utility/PickedFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace Задание__11.Assets.Utility
+{
+	internal static class PickedFile
+	{
+		public static string EncodingStringFormat { get; set; } = String.Empty;
+		public static Encoding LeadToEncoding { get; set; } = Encoding.Default;
+		public static string FileName { get; set; } = String.Empty;
+		public static string FileFullName { get; set; } = String.Empty;
+
+		private static readonly Dictionary<string, Action<RichTextBox>> _openActions = new Dictionary<string, Action<RichTextBox>>
+		{
+			["txt"] = richTextBox => richTextBox.AppendText(textData: File.ReadAllText(path: FileFullName, encoding: LeadToEncoding)),
+			["bin"] = richTextBox => richTextBox.AppendText(textData: Encoding.Default.GetString(bytes: File.ReadAllBytes(path: FileFullName))),
+			["rtf"] = richTextBox => richTextBox.Selection.Load(stream: File.Open(path: FileFullName, mode: FileMode.OpenOrCreate), DataFormats.Rtf)
+		};
+
+		private static readonly Dictionary<string, Action<RichTextBox>> _saveActions = new Dictionary<string, Action<RichTextBox>>
+		{
+			["txt"] = richTextBox => File.WriteAllText(path: FileFullName, contents: GetRange(richTextBox: richTextBox).Text, encoding: LeadToEncoding),
+			["bin"] = richTextBox => File.WriteAllBytes(path: FileFullName, bytes: LeadToEncoding.GetBytes(s: GetRange(richTextBox: richTextBox).Text)),
+			["rtf"] = richTextBox => GetRange(richTextBox: richTextBox).Save(stream: File.OpenWrite(path: FileFullName), dataFormat: DataFormats.Rtf)
+		};
+
+		public static TextRange GetRange(RichTextBox richTextBox)
+			=> new TextRange(position1: richTextBox.Document.ContentStart, position2: richTextBox.Document.ContentEnd);
+
+		public static void Open(RichTextBox richTextBox)
+		{
+			richTextBox.Document.Blocks.Clear();
+			_openActions[FileName.Split(separator: '.').Last()](obj: richTextBox);
+		}
+
+		public static void Save(RichTextBox richTextBox)
+			=> _saveActions[FileName.Split(separator: '.').Last()](obj: richTextBox);
+	}
+}
